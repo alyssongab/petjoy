aac4b39e75445b900c35af1f4be2d7f41b4cf410
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-11 00:21:57 -0400

    fix: ajusta responsividade do botao agendamento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import { useState } from 'react';
 const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     return (
-        <header className="bg-yellow-100 shadow-sm">
-            <div className="container mx-auto px-4 py-3">
+        <header className="bg-amber-50 shadow-sm">
+            <div className="container mx-auto px-4 py-3 overflow-hidden">
                 <div className="flex items-center justify-around">
                     {/* Logo e Nome (Esquerda) */}
                     <div className="flex items-center space-x-2">
-                        <a href='#' className='flex items-center'>
-                            <div className="w-10 h-10 bg-amber-100 rounded-full flex items-center justify-center">
+                        <a href='#' className='flex items-center hover:scale-110 transition-all duration-300'>
+                            <div className="w-10 h-10 bg-amber-50 rounded-full flex items-center justify-center">
                                 <img src="/logo.png" alt="PetJoy Logo" className='w-6 md:w-8 object-contain'/>
                             </div>
                             <span className="text-petbrown font-bold text-2xl sm:text-3xl " style={{ fontFamily: 'Lilita One, sans-serif' }}>
@@ -22,9 +22,9 @@ const Header = () => {
                     {/* Navegação (Centro) - Visível apenas em desktop */}
                     <nav className="hidden md:block">
                         <ul className="flex space-x-11">
-                            <li><a href="#servicos" className="text-petbrown hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>Serviços</a></li>
-                            <li><a href="#faq" className="text-petbrown hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>FAQ</a></li>
-                            <li><a href="#localizacao" className="text-petbrown hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>Localização</a></li>
+                            <li><a href="#servicos" className="text-petbrown text-lg hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>Serviços</a></li>
+                            <li><a href="#faq" className="text-petbrown text-lg hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>FAQ</a></li>
+                            <li><a href="#localizacao" className="text-petbrown text-lg hover:text-amber-600 font-medium transition-all duration-300 hover:scale-110 origin-center inline-block transform" style={{ fontFamily: 'Montserrat, sans-serif' }}>Localização</a></li>
                         </ul>
                     </nav>
 
@@ -38,7 +38,7 @@ const Header = () => {
 
                     {/* Área de Menu Mobile */}
                     <div className="md:hidden flex space-x-4">
-                        <button className="bg-[#1F9B3A] hover:bg-green-400 text-white px-4 py-1.5 md:px-5 md:py-2 text-sm md:text-base rounded-full font-medium transition-colors flex items-center space-x-1" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+                        <button className="max-[380px]:hidden bg-[#1F9B3A] hover:bg-green-400 text-white px-4 py-1.5 md:px-5 md:py-2 text-sm md:text-base rounded-full font-medium transition-colors flex items-center space-x-1" style={{ fontFamily: 'Montserrat, sans-serif' }}>
                             <img src="/wpp.png" alt="Whatsapp Logo" className='w-[5vw]'/>
                             <span>Agendamento</span>
                         </button>
@@ -62,6 +62,7 @@ const Header = () => {
                     <li><a href="#servicos" className="block text-gray-700 py-2" style={{ fontFamily: 'Montserrat, sans-serif' }}>Serviços</a></li>
                     <li><a href="#faq" className="block text-gray-700 py-2" style={{ fontFamily: 'Montserrat, sans-serif' }}>FAQ</a></li>
                     <li><a href="#localizacao" className="block text-gray-700 py-2" style={{ fontFamily: 'Montserrat, sans-serif' }}>Localização</a></li>
+                    <li><a href="#" className="min-[380px]:hidden text-gray-700 py-2" style={{ fontFamily: 'Montserrat, sans-serif' }}>Agendamento</a></li>
                 </ul>
             </div>
         </header>
