eb645e0de825d5e4850a0d1555709b598889c7ec
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-23 19:03:51 -0400

    style: melhora responsividade da descricao do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,6 @@ const Card = ({titulo, imgUrl, desc, botao}) => {
                 {/* Container do titulo do card */}
                 <div id="box-titulo" className="text-2xl md:text-3xl lg:text-4xl">
                     <h2 className="text-center">{titulo}</h2>
-                    {/* <h2>titulo</h2> */}
                 </div>
 
                 {/* Container da imagem */}
@@ -16,19 +15,16 @@ const Card = ({titulo, imgUrl, desc, botao}) => {
                     src={imgUrl} 
                     alt={titulo}
                     className=""/>
-                    {/* <p>imagem</p> */}
                 </div>
                 
                 {/* Container da descricao do card */}
-                <div id="box-desc" className="">
-                    <p className="sm:p-3 font-medium text-xs sm:text-sm md:text-base lg:text-lg text-center">{desc}</p>
-                    {/* <p>descricao</p> */}
+                <div id="box-desc" className="sm:max-md:w-[70vw] text-center p-2">
+                    <p className="text-sm sm:text-base md:text-lg 2xl:text-xl">{desc}</p>
                 </div>
 
                 {/* Container do botao do card */}
                 <div id="box-botao" className="">
                     <button className="bg-petbrown text-slate-50 p-1 sm:p-2 text-[0.50em] sm:text-base wider:text-xl w-[15vw] wider:w-72 wider:rounded-3xl rounded-2xl hover:scale-105">{botao}</button>
-                    {/* <button>botao</button> */}
                 </div>
             </div>
         </div>
