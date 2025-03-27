261d5918be42dbcf602abda4f131d0a65377b899
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-27 16:09:14 -0400

    feat: configura useState para abrir respostas na secao FAQ

--- src/components/FaqBox.jsx
+++ src/components/FaqBox.jsx
@@ -1,12 +1,25 @@
+import { CirclePlus} from 'lucide-react';
+import { useState } from 'react';
+
 const FaqBox = ({ pergunta, resposta }) => {
+
+    const[abrirResposta, setAbrirResposta] = useState(false);
+
+    function handleResposta(){
+        setAbrirResposta(!abrirResposta);
+    }
+
     return(
-        <div>
+        <div className="p-3">
 
-            <div id="pergunta">
-                
+            <div id="pergunta" className="bg-[#FFF188] rounded-lg">
+                <div className="flex flex-row justify-between items-center p-2">
+                    <p className="font-semibold text-sm w-[80vw]">{pergunta}</p>
+                    <button onClick={handleResposta}><CirclePlus className='cursor-pointer'/></button>  
+                </div>
             </div>
-            <div id="resposta">
-
+            <div id="resposta" className={`bg-[#FFF8C2] p-2 mt-[-3px] ${abrirResposta ? 'block' : 'hidden'}`}>
+                {resposta}
             </div>
         </div>
     )
--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -13,16 +13,25 @@ const Faq = () => {
             {/* Componente: Pergunta + Resposta */}
             <div id="faq-box">
                 <FaqBox
-                    pergunta=""
-                    resposta=""
+                    pergunta="Quais tipos de pets vocês atendem?"
+                    resposta="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis et id dolore ab asperiores iusto. Natus eveniet similique veritatis at, obcaecati minima quos provident dolores est dignissimos beatae cumque in."
                 />
                 <FaqBox
-                    pergunta=""
-                    resposta=""
+                    pergunta="Preciso agendar com antecedência para serviços de banho e tosa?"
+                    resposta="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis et id dolore ab asperiores iusto. Natus eveniet similique veritatis at, obcaecati minima quos provident dolores est dignissimos beatae cumque in."
                 />
-                <FaqBox/>
-                <FaqBox/>
-                <FaqBox/>
+                <FaqBox
+                    pergunta="Vocês oferecem atendimento veterinário emergencial?"
+                    resposta="orem, ipsum dolor sit amet consectetur adipisicing elit. Corporis et id dolore ab asperiores iusto. Natus eveniet similique veritatis at, obcaecati minima quos provident dolores est dignissimos beatae cumque in."
+                />
+                <FaqBox
+                    pergunta="Posso deixar meu pet hospedado com vocês durante viagens longas?"
+                    resposta="orem, ipsum dolor sit amet consectetur adipisicing elit. Corporis et id dolore ab asperiores iusto. Natus eveniet similique veritatis at, obcaecati minima quos provident dolores est dignissimos beatae cumque in."
+                />
+                <FaqBox
+                    pergunta="Como funciona o serviço de passeios para cães?"
+                    resposta="orem, ipsum dolor sit amet consectetur adipisicing elit. Corporis et id dolore ab asperiores iusto. Natus eveniet similique veritatis at, obcaecati minima quos provident dolores est dignissimos beatae cumque in."
+                />
             </div>
 
        </section>
