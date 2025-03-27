5f2fd2040c1d37af52d70d5f7b19455342973d1e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-27 16:29:33 -0400

    style: adiciona animacao no box de resposta (faq)

--- src/components/FaqBox.jsx
+++ src/components/FaqBox.jsx
@@ -1,4 +1,4 @@
-import { CirclePlus} from 'lucide-react';
+import { CircleMinus, CirclePlus} from 'lucide-react';
 import { useState } from 'react';
 
 const FaqBox = ({ pergunta, resposta }) => {
@@ -15,10 +15,18 @@ const FaqBox = ({ pergunta, resposta }) => {
             <div id="pergunta" className="bg-[#FFF188] rounded-lg">
                 <div className="flex flex-row justify-between items-center p-2">
                     <p className="font-semibold text-sm w-[80vw]">{pergunta}</p>
-                    <button onClick={handleResposta}><CirclePlus className='cursor-pointer'/></button>  
+                    <button onClick={handleResposta}>
+                        {!abrirResposta ? 
+                        <CirclePlus className='cursor-pointer'/>
+                        :
+                        <CircleMinus className='cursor-pointer'/>
+                        }
+                        </button>  
                 </div>
             </div>
-            <div id="resposta" className={`bg-[#FFF8C2] p-2 mt-[-3px] ${abrirResposta ? 'block' : 'hidden'}`}>
+            <div id="resposta" 
+                className={`bg-[#FFF8C2] p-2 mt-[-5px] overflow-hidden transition-all duration-300 ease-in-out
+                ${abrirResposta ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0 p-0'}`}>
                 {resposta}
             </div>
         </div>
