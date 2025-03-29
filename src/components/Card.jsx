f836979ec575d2bbd3b65688072e688fe57e7a65
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-03-29 17:41:01 -0400

    style: justify between para telas maiores

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({titulo, imgUrl, desc, botao}) => {
     return(
         <div id="card" className="bg-slate-50 flex flex-col rounded-2xl p-2 shadow-xl w-[85vw] h-[55vh] sm:h-[50vh] md:w-[60vw] md:h-[40vh] lg:w-[40vw] lg:h-[80vh] 2xl:w-[30vw] 2xl:h-[70vh] hover:scale-105 transition-all duration-300">
             {/* titulo, img, desc, button */}
-            <div id="conteudo-card" className="md:p-4 h-full flex flex-col items-center justify-evenly text-center">
+            <div id="conteudo-card" className="md:p-4 h-full flex flex-col items-center justify-evenly xl:justify-between text-center">
 
                 {/* Container do titulo do card */}
                 <div id="box-titulo" className="text-xl md:text-3xl lg:text-3xl xl:text-4xl">
