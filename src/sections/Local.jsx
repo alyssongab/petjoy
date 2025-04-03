c456927aa62076d8583c14977dc72d65d66b3713
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-04-02 20:40:35 -0400

    style: diminui o tamanho do iframe

--- src/sections/Local.jsx
+++ src/sections/Local.jsx
@@ -12,24 +12,36 @@ const Local = () => {
         {/* Div que controla as duas colunas (desktop) ou 2 linhas (mobile) */}
         <div className='p-3 lg:p-6 lg:grid lg:grid-cols-2 justify-center items-center'>
             {/* Iframe da localizacao */}
-            <div id="mapa" className="p-3 lg:p-6 lg:grid lg:grid-cols-2 justify-center items-center">
+            <div
+                style={{
+                    position: 'relative',
+                    overflow: 'hidden',
+                    paddingBottom: '56.25%', /* 16:9 Aspect Ratio */
+                    height: 0,
+                }}
+                >
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6699.840022953697!2d-60.01031221505367!3d-3.1329386371643206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c057715e51dc1%3A0x131a0659382fadd9!2sFederal%20Institute%20of%20Education%2C%20Science%20and%20Technology%20of%20Amazonas!5e0!3m2!1sen!2sbr!4v1743638074163!5m2!1sen!2sbr"
-                    width="600"
-                    height="400"
+                    style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    border: '1px solid black',
+                    }}
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
-                    style={{ border: '1px solid black' }}
                 ></iframe>
             </div>
 
             {/* Texto da localizacao */}
-            <div className='flex text-center justify-center items-center w-[80%]'>
+            <div className='flex text-center justify-center items-center w-[100%] p-4'>
                 <div>
                     <img src="/pin.png" alt="Localization" width={'150px'}/>
                 </div>
-                <p className='text-xl'>
+                <p className='text-base'>
                     Estamos localizados na rua xxx, numero xx, proximo ao (ponto de referencia). Faça um <a href='https://wa.me/5592988016664' target='_blank' className='text-amber-800 underline'>agendamento</a> ou venha trazer seu pet para uma visita!
                 </p>
             </div>
